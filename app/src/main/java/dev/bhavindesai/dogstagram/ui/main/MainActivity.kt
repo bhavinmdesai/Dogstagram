222033bf5e597dbb5c3f4087a798da2ff331a8ef
Bhavin Desai <bhavinmdesai@gmail.com>
2021-03-22 14:10:52 +0530

    Implemented toolbar navigation and screen animation

--- app/src/main/java/dev/bhavindesai/dogstagram/ui/main/MainActivity.kt
+++ app/src/main/java/dev/bhavindesai/dogstagram/ui/main/MainActivity.kt
@@ -1,12 +1,33 @@
 package dev.bhavindesai.dogstagram.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dev.bhavindesai.dogstagram.R
+import dev.bhavindesai.dogstagram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
